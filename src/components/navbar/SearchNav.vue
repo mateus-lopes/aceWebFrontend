ecbb3d56c6e576cb8309e1deacdbe68c1922a370
Mateus Lopes Albano <mateusalbano22@gmail.com>
2023-11-15 13:01:19 -0300

    adicioando sub menu de pesquisa

--- src/components/navbar/SearchNav.vue
+++ src/components/navbar/SearchNav.vue
@@ -1,38 +1,48 @@
 <template>
-  <div class="mr-2 flex justify-between items-center text-zinc-600 cursor-pointer">
-    <MagnifyingGlassIcon @click="toogleSearch()" class="h-6 w-6" aria-hidden="true" />
-    <input :class="{ show: !search }" type="text" v-model="input" placeholder="Pesquisar..." />
+  <div class="ml-2 flow-root lg:ml-4">
+    <div>
+      <!-- CONECTED -->
+      <button class="flex items-center">
+        <span class="sr-only">Open user menu</span>
+        <MagnifyingGlassIcon @mouseover="search = true" class="h-6 w-6 flex-shrink-0 hover:text-black" aria-hidden="true" />
+      </button>
+    </div>
+    <PopUpNav 
+      :open="search"
+      class="w-96"
+      @mouseleave="search = false"
+    >
+      <div class="flex">
+        <input class="bg-slate-50 border" type="text" v-model="input" placeholder="Digite o texto aqui..." />
+        <BtnLinkSm class="border-primary" title="Buscar" @click="toogleSearch()" />
+      </div>
+      <hr>
+      <p class="text-slate-500">Você ainda não tem pesquisas recentes</p>
+      <!-- <p class="text-slate-500">Ultimas Pesquisas...</p> -->
+    </PopUpNav>
+    <span class="sr-only">view profile</span>
   </div>
 </template>
 
 <script setup>
 import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
-
 import { ref } from 'vue'
+import BtnLinkSm from '@/components/BtnLinkSm.vue'
+import PopUpNav from './PopUpNav.vue';
+
+const search = ref(false)
 
 let input = ref('')
-
-const search = ref(true)
-
 function toogleSearch() {
-  if (input.value == '') {
-    search.value = !search.value
-  } else {
-    input.value = ''
-  }
+  input.value = ''
 }
 </script>
 
 <style scoped>
 input {
-  animation: normal 0.5s ease-in-out 0s 1 fadeOut;
   display: block;
-  width: 0px;
-  padding: 10px 0px;
-  font-size: 16px;
-  border: none;
-  /* color: rgb(55 65 81 / var(1)); */
-  border-bottom: 1px solid #a3a3a3;
+  width: 100%;
+  padding: 10px 16px;
 }
 
 input:hover {
@@ -43,38 +53,5 @@ input:focus,
 input:active {
   border-bottom: 1px solid #000;
   outline: none;
-}
-
-input.show {
-  width: 250px;
-  margin-left: 10px;
-  padding: 10px 16px;
-  animation: normal 0.5s ease-in-out 0s 1 fadeIn;
-}
-
-@keyframes fadeIn {
-  from {
-    width: 0px;
-    margin-left: 10px;
-    padding: 10px 0px;
-  }
-  to {
-    width: 250px;
-    margin-left: 10px;
-    padding: 10px 16px;
-  }
-}
-
-@keyframes fadeOut {
-  from {
-    width: 250px;
-    margin-left: 10px;
-    padding: 10px 16px;
-  }
-  to {
-    width: 0px;
-    margin-left: 10px;
-    padding: 10px 0px;
-  }
 }
 </style>
